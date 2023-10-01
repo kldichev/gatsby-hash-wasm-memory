f6d6b199ccc54367447b8bea26d425e7dd04c157
kldichev <146625963+kldichev@users.noreply.github.com>
2023-10-01 13:43:36 +0200

    fist commit

---
+++ gatsby-config.ts
@@ -0,0 +1,22 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-articles`,
+        path: `${__dirname}/src/articles`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+};
+
+export default config;
